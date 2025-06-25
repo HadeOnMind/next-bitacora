8e0089ecb6af6249828a559698aa726833fb25c9
HadeOnMin <jhojanr11.jr@gmail.com>
2025-06-25 00:25:48 -0500

    toggle state with tailwind

--- app/workspace/dev/page.tsx
+++ app/workspace/dev/page.tsx
@@ -1,4 +1,18 @@
+"use client"
+import { useState } from 'react';
+
+
+
 export default function DevPage() {
+
+const [IsSelected, setIsSelected] = useState(false);
+
+const ToggleSelection = () => {
+setIsSelected(!IsSelected);
+if(IsSelected){
+  alert("SELECCIONADO")
+}
+};
 
 
 
@@ -14,12 +28,65 @@ export default function DevPage() {
       <div className="pt-8 bg-amber-500 grid grid-cols-2 gap-6">
 
 
-        <div className="bg-red-400">card</div>
-        <div className="bg-red-400">card</div>
-        <div className="bg-red-400">card</div>
-        <div className="bg-red-400">card</div>
-        <div className="bg-red-400">card</div>
-        <div className="bg-red-400">card</div>
+        <div className="bg-red-400 rounded-xl ">
+
+          <div  onClick={ToggleSelection} className="text-center hover:bg-amber-300 ${
+        isSelected ? 'bg-green-600' : 'bg-gray-400'
+      }`" >
+        
+        {IsSelected ? 'Selected' : 'Not Selected'}
+
+          </div>
+
+        </div>
+
+        <div className="bg-red-400 rounded-xl ">
+
+          <div className="text-center hover:bg-amber-300">
+
+            inside card 
+
+          </div>
+
+        </div>
+
+        <div className="bg-red-400 rounded-xl ">
+
+          <div className="text-center hover:bg-amber-300">
+
+            inside card 
+
+          </div>
+
+        </div>
+        <div className="bg-red-400 rounded-xl ">
+
+          <div className="text-center hover:bg-amber-300">
+
+            inside card 
+
+          </div>
+
+        </div>
+        <div className="bg-red-400 rounded-xl ">
+
+          <div className="text-center hover:bg-amber-300">
+
+            inside card 
+
+          </div>
+
+        </div>
+        <div className="bg-red-400 rounded-xl ">
+
+          <div className="text-center hover:bg-amber-300">
+
+            inside card 
+
+          </div>
+
+        </div>
+        
 
 
 
@@ -31,6 +98,13 @@ page2
       </div>
 
 
+
+      <div className="flex bg-blue-400 rounded-xl mt-12 gap-8 items-center justify-center-safe">
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Merge</button>
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Type Image</button>
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Type Text</button>
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Type Canvas</button>
+      </div>
 
 
     </div>
