f0cba12d256ede70c31935fbc50c8ed5f34b22d4
Jhojan Restrepo Pareja <jpareja@bpogs.com>
2025-07-07 16:41:20 -0500

    navbar update and small replacements

--- app/components/hnavbar.tsx
+++ app/components/hnavbar.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { twMerge } from 'tailwind-merge'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function HorzNavbar() {
   const navItems = [
@@ -10,6 +14,11 @@ export default function HorzNavbar() {
     { label: 'dev', href: '/workspace/dev' },
   ];
 
+  const pathname = usePathname();
+
+/*`text-gray-600 hover:text-blue-600 transition ${pathname === item.href && "text-blue-700"} ` */
+
+
   return (
     <nav className="bg-white shadow-md w-full fixed top-0 left-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -17,7 +26,7 @@ export default function HorzNavbar() {
           <div className="text-xl font-bold text-blue-600">My Bitacora</div>
           <div className="hidden md:flex space-x-6">
             {navItems.map((item) => (
-              <Link key={item.href} href={item.href} className="text-gray-700 hover:text-blue-600 transition">
+              <Link key={item.href} href={item.href} className={twMerge("text-gray-700 hover:text-blue-600 transition", pathname === item.href && "text-blue-700 italic tracking-wide drop-shadow-[0_0_8px_rgba(59,130,246,0.8)] transition-all duration-300")}>
                 {item.label}
               </Link>
             ))}
