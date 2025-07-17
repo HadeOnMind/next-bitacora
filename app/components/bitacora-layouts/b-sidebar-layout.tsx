80ee3f87ab3cc34b66e49ca099c780d4c03773c8
HadeOnMin <jhojanr11.jr@gmail.com>
2025-07-16 22:41:27 -0500

    workspace advances - preliminar

--- app/components/bitacora-layouts/b-sidebar-layout.tsx
+++ app/components/bitacora-layouts/b-sidebar-layout.tsx
@@ -7,8 +7,8 @@ export default function Sidebar() {
   const [open, setOpen] = useState(true);
 
   return (
-    <aside className={`bg-slate-800 text-white transition-all duration-300 ${open ? 'w-56' : 'w-12'} p-2 my-16`}>
-      <button onClick={() => setOpen(!open)} className={"mb-4 text-cyan-400 hover:bg-cyan-800 rounded-xl"}>
+    <aside className={`bg-cyan-500 text-white transition-all duration-300 h-screen ${open ? 'w-56' : 'w-12'} p-2 my-16 z-50 shadow-lg`}>
+      <button onClick={() => setOpen(!open)} className={"mb-4 p-1 text-cyan-400 hover:bg-cyan-800 rounded-xl flex justify-center"}>
         {open ? <HiChevronLeft /> : <HiChevronRight />}
       </button>
 
